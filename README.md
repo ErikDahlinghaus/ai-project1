1dd456bc1ced456ece4387338f075344d529eae7
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2013-09-04 21:53:08 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CS4341 Project 1
+===========
+
+Plays Connect 4 using minimax and alpha-beta pruning
---
+++ src/connectn/GameState.java
@@ -0,0 +1,44 @@
+package connectn;
+
+import java.io.*;
+
+public class GameState {
+	
+	
+	int N=4;
+	
+	char NO_PLAYER = 'x';
+	char PLAYER1 = 'a';
+	char PLAYER2 = 'b';
+	
+	int height;
+	int width;
+	char [][] board;
+	
+	
+	
+	GameState(int height, int width, int N)
+	{
+		this.height = height;
+		this.width = width;
+		this.N = N;
+		
+		initBoard();
+	}
+	
+	private void initBoard(){
+		
+		int i,j;
+		
+		for( i=0; i<height; i++ ){
+			for( j=0; j<width; j++ ){
+				this.board[i][j] = NO_PLAYER;
+			}
+		}
+		
+	}
+	
+	
+	
+	
+}
